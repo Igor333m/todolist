8a327de00673c312ff81f53d98794cd536b6cf4b
Igor Todorovic <igor333m@yahoo.com>
2019-02-27 08:47:19 -0800

    feat: Add ListItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import AddItems from './components/AddItems';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div>Yo</div>
+      <AddItems></AddItems>
     );
   }
 }
---
+++ src/components/AddItems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddItems extends Component {
+  render() {
+    return(
+      <div>Yo</div>
+    );
+  }
+}
+
+export default AddItems;
